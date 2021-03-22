1faaabd1c62dd7047768aee2f08e5c36fdb2a359
Adrian1426 <adrianhh1426@gmail.com>
2021-03-21 19:27:01 -0600

    before y after

--- test-parte1/src/test/java/ahh/com/testparte1/suma/SumaTest.java
+++ test-parte1/src/test/java/ahh/com/testparte1/suma/SumaTest.java
@@ -5,6 +5,8 @@ package ahh.com.testparte1.suma;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -14,12 +16,23 @@ import org.junit.jupiter.api.Test;
 public class SumaTest {
 
 	Suma suma = new Suma();
+	
+	@BeforeEach
+	public void before() {
+		System.out.println("ejecución before");
+	}
+	
+	@AfterEach
+	public void after() {
+		System.out.println("ejecución after");
+	}
 
 	@Test
 	public void sumaTest() {
 
 		int resultadoSumaObtenido = suma.suma(10, 15);
 		int resultadoSumaEsperado = 25;
+		System.out.println("ejecución suma");
 
 		assertEquals(resultadoSumaEsperado, resultadoSumaObtenido);
 
